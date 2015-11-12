304cda97a88404d46a660b521177802a209489a6
Jyrone Parker <jyrone.parker@gmail.com>
2015-11-12 15:17:32 -0800

    added controller

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,52 @@
+<?php namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use DB;
+use Request;
+
+class FileController extends BaseController
+{
+    public function saveFile()
+    {
+        $file = Request::file('file');
+	$name = $file->getClientOriginalName();
+	$name = str_replace(' ', '_', $name);
+        Storage::put($name,  file_get_contents($file->getRealPath()));
+        DB::table('files')->insert(
+    ['name' => $name ]
+);
+        return response()->json('success');
+    }
+
+
+    public function deleteFile($name)
+    {
+        Storage::delete($name);
+        return response()->json('success');
+    }
+
+    public function getFileList(){
+
+        $files = Storage::files('/');
+	$response = [];
+	foreach($files as $file){
+	array_push($response,[
+	'file' => $file
+	]);
+	}
+        return response()->json($response);
+
+    }
+
+    public function viewFile($name){
+
+        return response()->make(Storage::get($name), 200, [
+            'Content-Type' => Storage::mimeType($name),
+            'Content-Disposition' => 'inline; '.$name,
+        ]);
+
+    }
+
+}
